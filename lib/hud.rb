d629b5b8e9455c70cf3545ac96f834bafe9d50f8
Michael Tighe <strideriidx@gmail.com>
2015-01-12 01:04:25 -0600

    Add score multiplier to hud

--- lib/hud.rb
+++ lib/hud.rb
@@ -1,6 +1,7 @@
 class Hud
     WARP_READY_COLOR    = 0xFF859900
     WARP_CHARGING_COLOR = 0xFFD23D3D
+    MULTIPLIER_COLOR    = 0xFF96AA11
 
     def initialize(window, play_state, player)
         @window = window
@@ -9,11 +10,13 @@ class Hud
         @warp_bar = ProgressBar.new(@window, 445, 441, 96, 16)
         @font = Gosu::Font.new(window, 'Courier New', 40)
         @player_icon = window.load_image('player.png')
+        @multiplier_color = Gosu::Color.argb(MULTIPLIER_COLOR)
     end
 
     def update
         @score = @state.score.to_s.rjust(6, '0')
         @lives = "x#{@state.lives.to_s.rjust(2, '0')}"
+        @multiplier = "#{@state.multiplier}x"
         @warp_bar.value = @player.warp_status
         @warp_bar.color_fg = warp_bar_color
     end
@@ -21,6 +24,7 @@ class Hud
     def draw
         @window.fill_rect(*bounds, Gosu::Color::BLACK)
         @font.draw(@score, 10, 435, 0, 1.0, 1.0)
+        @font.draw(@multiplier, 130, 435, 0, 1.0, 1.0, @multiplier_color)
         @font.draw(@lives, 580, 435, 0, 1.0, 1.0)
         @window.draw_line(*location, Gosu::Color::GRAY, @window.width, PlayState::FLOOR, Gosu::Color::GRAY)
         @player_icon.draw(555, 436,  0, 0.5, 0.5)
